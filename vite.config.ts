f92162aedad23de74a5e8fa51e756d5e669401c3
Naz Malik <nazmalik08@gmail.com>
2023-12-04 13:31:30 +0000

    update buffer thing in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import Markdown from "vite-plugin-md";
+import { Buffer } from "buffer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), Markdown()],
   assetsInclude: ["**/*.md"],
   define: {
-    global: {}, // Polyfill for global if needed
-    Buffer: ["buffer", "Buffer"], // Polyfill for Buffer
+    global: "window", // Define 'global' if needed
+    Buffer: Buffer, // Correctly reference the Buffer polyfill
   },
   resolve: {
     alias: {
-      // This alias is necessary in Vite environments
-      buffer: "buffer",
+      buffer: "buffer", // Ensure the 'buffer' package is aliased
     },
   },
 });
