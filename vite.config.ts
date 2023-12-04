49311b7fefc873fe3cd82626046d501f497de132
Naz Malik <nazmalik08@gmail.com>
2023-12-04 13:37:47 +0000

    at least dont break everything

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import Markdown from "vite-plugin-md";
-import { Buffer } from "buffer";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), Markdown()],
   assetsInclude: ["**/*.md"],
   define: {
-    global: "window", // Define 'global' if needed
-    "process.env": {}, // Mock 'process.env' if needed
-    "process.browser": true, // Define 'process.browser' if using libraries that check for this
-    Buffer: "Buffer", // Correctly reference the Buffer global variable as a string
+    global: {}, // Polyfill for global if needed
+    Buffer: ["buffer", "Buffer"], // Polyfill for Buffer
   },
   resolve: {
     alias: {
-      buffer: "buffer", // Ensure the 'buffer' package is aliased
+      // This alias is necessary in Vite environments
+      buffer: "buffer",
     },
   },
 });
