f5f172bf1613dfc5fc829a59c5a25bacba215014
Naz Malik <nazmalik08@gmail.com>
2023-12-10 15:12:57 +0000

    try ontouchstart event

--- src/Metronomes/PlayButton.tsx
+++ src/Metronomes/PlayButton.tsx
@@ -6,12 +6,18 @@ type PropsT = {
 };
 
 const PlayButton: React.FC<PropsT> = ({ isPlaying, onToggle }) => {
+  const handleInteraction = (event: React.MouseEvent | React.TouchEvent) => {
+    event.preventDefault(); // Prevents double firing of events
+    onToggle();
+  };
+
   return (
     <button
       className={`btn-lg w-48 btn btn-active ${
         isPlaying ? "btn-accent" : "btn-primary"
       }`}
-      onClick={onToggle}
+      onClick={handleInteraction}
+      onTouchStart={handleInteraction} // Using touchstart instead of touchend
     >
       {isPlaying ? "Stop" : "Start"}
     </button>
