4fe465b48db80b4e03f134460adda50c1bec0d89
Naz Malik <nazmalik08@gmail.com>
2024-09-13 16:48:39 +0100

    pass the beat

--- src/Home/projects.tsx
+++ src/Home/projects.tsx
@@ -16,6 +16,11 @@ export const projects: ProjectT[] = [
     link: "https://www.tempotick.com",
   },
   {
+    title: "Pass the Beat",
+    description: "Bing bong bing bong",
+    link: "https://pass-the-beat.vercel.app/",
+  },
+  {
     title: "Star Wars Facts",
     description: "Alexa skill that gets traffic spikes on May 4th",
     link: "https://www.amazon.co.uk/nazwhale-Star-Wars-Facts/dp/B0725WTNWZ/ref=sr_1_5?dchild=1&keywords=star+wars+facts&qid=1589405211&s=digital-skills&sr=1-5",
